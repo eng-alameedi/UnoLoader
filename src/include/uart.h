fafd6495fe1c518cdb21af6cea76782297ba63a0
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-21 23:16:23 +0300

    update the uart file settings

--- src/include/uart.h
+++ src/include/uart.h
@@ -10,6 +10,12 @@
 #ifndef _UART_H_
 #define _UART_H_
 
-#include "settings.h"
+#include <stdint.h>
+
+void uart_init(void);
+
+void uart_send(uint8_t);
+
+uint8_t uart_receive(void)
 
 #endif  // _UART_H_
