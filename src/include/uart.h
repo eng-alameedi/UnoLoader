34b5b11a6de791bef280693b8a5dec1f62c07da1
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-21 22:42:16 +0300

    add new header files for uart and interrupt control

---
+++ src/include/uart.h
@@ -0,0 +1,15 @@
+//
+// File: uart.h
+//
+// This file contain the main register,
+// and functions to initialize connection,
+// between the user terminal and the MCU,
+// to upload the application code.
+//
+
+#ifndef _UART_H_
+#define _UART_H_
+
+// code here
+
+#endif  // _UART_H_
