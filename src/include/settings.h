5c067ae335b313a39b8a907fc4bd692fbfc93e5a
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-21 23:12:25 +0300

    add a settings.h file include the main bootloader settings

---
+++ src/include/settings.h
@@ -0,0 +1,15 @@
+//
+// File: settings.h
+//
+// This file contain the main settings of the,
+// bootloader program, if you need to change,
+// settings like baud rate, cpu frequency or,
+// other do it here please.
+//
+
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+#define BAUD (9600)  // initial baud rate
+
+#endif  // _SETTINGS_H_
