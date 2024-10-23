3e30d8c0b4006bae60cda9fec9a85e4ddfd60fea
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-23 23:32:07 +0300

    update the main bootloader code

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,19 @@
 // The main file for ATmega328p BootLoader
 //
 
-#include "loader_core.h"
+#include "bootloader.h"
+#include "settings.h"
+#include "uart.h"
 
 int main() {
-  // code here
+  if (_MEM_8(MCUSR) & _BV(EXTRF)) {
+    uart_init();
+    uart_send('B');
+    while (true) {
+      // code for receive and write the code to memory
+    }
+  } else {
+    // code to cancel the bootloader, and go to application code.
+  }
   return 0;
 }
