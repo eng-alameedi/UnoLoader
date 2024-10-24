ae179108416c898c1abc0d8de43e4861e21b3ad0
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-25 00:47:00 +0300

    add the ctc_setup() to main bootloader code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "bootloader.h"
 #include "interrupt.h"
 #include "settings.h"
+#include "timer.h"
 #include "uart.h"
 #include "watchdog.h"
 
@@ -15,6 +16,7 @@ int main() {
     cli();                                  // disable the external interrupt
     uart_init();                            // initialize the uart communication
     uart_send('B');                         // send a 'B' to the terminal to indicate bootloade
+    ctc_setup();                            // setup the timer (ctc mode), and delay(ms) function
     set_wdt();                              // activate the watchdog timer for 1s.
     while (!(_MEM_8(UCSR0A) & _BV(RXC0)));  // infinite loop to check if there is a received code
     while (true) {
