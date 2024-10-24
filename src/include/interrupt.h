cf7137db835f8b0d0660ae1906c1d88dd82b52ac
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-24 16:58:33 +0300

    add jump to application inline asm code

--- src/include/interrupt.h
+++ src/include/interrupt.h
@@ -16,4 +16,8 @@
 #define sei() __asm__ __volatile__("sei ::: memory")
 #endif  // sei()
 
+#ifndef jmpapp
+#define jmpapp() __asm__ __volatile__("jmp 0x0000")
+#endif  // jmpapp()
+
 #endif  // _LOADER_CORE_H_
