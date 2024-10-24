1f775a226c6ca2b5c2d5aa8bbc87aff32a7a7d54
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-24 16:22:33 +0300

    update the main bootloader file

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,24 @@
 //
 
 #include "bootloader.h"
+#include "interrupt.h"
 #include "settings.h"
 #include "uart.h"
+#include "watchdog.h"
 
 int main() {
   if (_MEM_8(MCUSR) & _BV(EXTRF)) {
+    cli();
     uart_init();
     uart_send('B');
+    set_wdt();
     while (true) {
       // code for receive and write the code to memory
+    }
+    sei();
+
+    while (true) {
+      // infinite loop
     }
   } else {
     // code to cancel the bootloader, and go to application code.
